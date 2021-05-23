b5b94119ffee3028ff2a886fb56c5330d730f6f0
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2021-05-23 21:32:16 +0200

    Removed test folder

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,5 @@ module.exports = {
   ],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
 }
