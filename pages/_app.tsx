7e569f1efb5da062932dfe73418bcb928045ce65
segebre <10774915+Segebre@users.noreply.github.com>
2022-07-04 01:02:11 +0200

    Added code hightlight to post

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../static/styles.css';
 import type { AppProps } from 'next/app';
 import PageWrapper from '../src/pages/page-wrapper';
+import 'prismjs';
 
 const App = ({ Component, pageProps }: AppProps): React.ReactElement => {
   return (
