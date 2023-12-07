287e83420e0cb29b0f5187f1238b7cde76562499
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2023-12-07 22:14:40 +0100

    add missing reference

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 /**
  * Required for using native CSS nesting in CSS modules
  * The file and packages referenced below can be removed once the "Native CSS nesting support for css modules" issue is resolved
+ * Reference: https://github.com/vercel/next.js/issues/55053
  * 
  * Solution suggested by samir9saeedi 
  * Reference: https://github.com/vercel/next.js/issues/55053#issuecomment-1733946183
