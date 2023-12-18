3caef17bb7652e268c0ab87861f396e5b5682c9d
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2023-12-18 23:30:11 +0100

    Improve CSS support (#29)
    * add documentation for `@value` and `composes`
    
    * add and configure base stylelint
    
    * update stylelint rules
    
    support CSS Modules standards and features
    
    * fix stylelint issues
    
    * extend scripts to support stylelint
    
    * add and configure stylelint-config-recess-order
    
    * run stylint fix
    
    fixes new defined order for CSS styles
    
    * ran format

--- postcss.config.js
+++ postcss.config.js
@@ -2,28 +2,28 @@
  * Required for using native CSS nesting in CSS modules
  * The file and packages referenced below can be removed once the "Native CSS nesting support for css modules" issue is resolved
  * Reference: https://github.com/vercel/next.js/issues/55053
- * 
- * Solution suggested by samir9saeedi 
+ *
+ * Solution suggested by samir9saeedi
  * Reference: https://github.com/vercel/next.js/issues/55053#issuecomment-1733946183
- * 
+ *
  * Based on Next.js documentation for "Customizing Plugins"
  * Reference: https://nextjs.org/docs/pages/building-your-application/configuring/post-css#customizing-plugins
  */
 module.exports = {
-  "plugins": [
-    "postcss-nesting",
-    "postcss-flexbugs-fixes",
+  plugins: [
+    'postcss-nesting',
+    'postcss-flexbugs-fixes',
     [
-      "postcss-preset-env",
+      'postcss-preset-env',
       {
-        "autoprefixer": {
-          "flexbox": "no-2009"
+        autoprefixer: {
+          flexbox: 'no-2009',
         },
-        "stage": 3,
-        "features": {
-          "custom-properties": false
-        }
-      }
-    ]
-  ]
-}
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
+};
