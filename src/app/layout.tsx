69212e86a26313833a605644404e39fa65a22f0e
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2024-01-02 23:05:15 +0100

    add app folder base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+import './global.css';
+import styles from './styles.module.css';
 import type { LayoutProps } from './types';
 
 const Layout = ({ children }: LayoutProps) => (
-  <html lang="en">
-    <body>{children}</body>
+  <html lang="en" className={styles.html}>
+    <body className={styles.body}>
+      <div className={styles.content}>{children}</div>
+    </body>
   </html>
 );
 
