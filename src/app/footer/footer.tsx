078bda3d7f8e3ad5832c8be43b42c81a5cbdf708
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2024-01-04 22:43:11 +0100

    add social links to footer

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -1,3 +1,4 @@
+import ExternalLink from '../../components/external-link';
 import { GitHubIcon, LinkedInIcon } from './assets';
 import styles from './styles.module.css';
 
@@ -5,8 +6,15 @@ const Footer = () => {
   return (
     <footer className={styles.container}>
       <div className={styles.socialsContainer}>
-        <GitHubIcon />
-        <LinkedInIcon />
+        <ExternalLink
+          href="https://github.com/segebre-dev/segebre-dev"
+          noA11yIcon
+        >
+          <GitHubIcon alt="Juan's GitHub page" />
+        </ExternalLink>
+        <ExternalLink href="https://de.linkedin.com/in/segebre" noA11yIcon>
+          <LinkedInIcon alt="Juan's LinkedIn page" />
+        </ExternalLink>
       </div>
       <p>Developed by Juan Enrique Segebre Zaghmout</p>
     </footer>
--- src/app/footer/test.tsx
+++ src/app/footer/test.tsx
@@ -1,10 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import Footer from '.';
 
-it('renders the footer element', () => {
+it('renders the footer landmark', () => {
   render(<Footer />);
 
   expect(screen.getByRole('contentinfo')).toBeInTheDocument();
+});
+
+it('renders the link to GitHub', () => {
+  render(<Footer />);
+
+  const githubLink = screen.getByRole('link', { name: /Juan's GitHub page/ });
+  expect(githubLink).toBeInTheDocument();
+  expect(githubLink).toHaveAttribute(
+    'href',
+    'https://github.com/segebre-dev/segebre-dev'
+  );
+});
+
+it('renders the link to the LinkedIn page', () => {
+  render(<Footer />);
+
+  const githubLink = screen.getByRole('link', {
+    name: /Juan's LinkedIn page/,
+  });
+  expect(githubLink).toBeInTheDocument();
+  expect(githubLink).toHaveAttribute(
+    'href',
+    'https://de.linkedin.com/in/segebre'
+  );
 });
 
 it('renders the author message', () => {
