b373f50c20c747fa3fd6aa22a8577379db18cbde
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2024-01-05 23:26:40 +0100

    move footer to folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,1 @@
-import './global.css';
-import Footer from './footer';
-import styles from './styles.module.css';
-import type { LayoutProps } from './types';
-
-const Layout = ({ children }: LayoutProps) => (
-  <html lang="en" className={styles.html}>
-    <body className={styles.body}>
-      <div className={styles.content}>{children}</div>
-      <Footer />
-    </body>
-  </html>
-);
-
-export default Layout;
+export { default } from './_layout';
