38211bf481dbf258bcdffaea08a87ec2340a6a8f
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2024-01-27 19:37:23 +0100

    move and style mdx components

---
+++ src/mdx-components/index.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import ExternalLink from '@/components/external-link';
+import styles from './styles.module.css';
+import type { MDXComponents } from 'mdx/types';
+
+// Define default values to adapt to expected types from useMDXComponents
+const styledA = ({
+  href = '#',
+  children = '',
+  className,
+  ...otherProps
+}: React.ComponentPropsWithoutRef<'a'>) => (
+  <ExternalLink
+    href={href}
+    className={clsx(className, styles.link)}
+    {...otherProps}
+  >
+    {children}
+  </ExternalLink>
+);
+
+const generateStyledHeading = (Heading: 'h2' | 'h3') => {
+  const generatedComponent = ({
+    children,
+    ...otherProps
+  }: React.ComponentPropsWithoutRef<typeof Heading>) => (
+    <Heading className={styles.heading} {...otherProps}>
+      {children}
+    </Heading>
+  );
+  generatedComponent.displayName = Heading;
+
+  return generatedComponent;
+};
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    a: styledA,
+    h2: generateStyledHeading('h2'),
+    h3: generateStyledHeading('h3'),
+    ...components,
+  };
+}
---
+++ src/mdx-components/styles.module.css.d.ts
@@ -0,0 +1,5 @@
+declare const styles: {
+  readonly heading: string;
+  readonly link: string;
+};
+export = styles;
