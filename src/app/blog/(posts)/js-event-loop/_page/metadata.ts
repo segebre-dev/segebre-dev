26de4ea8b64975ca7841f14a3a8c801f06319565
Juan Enrique Segebre Zaghmout <10774915+Segebre@users.noreply.github.com>
2024-02-15 21:49:34 +0100

    update metainformation on blog posts

--- src/app/blog/(posts)/js-event-loop/_page/metadata.ts
+++ src/app/blog/(posts)/js-event-loop/_page/metadata.ts
@@ -1,10 +1,11 @@
 import { Metadata } from 'next';
+import { BlogPostMetainformation } from './types';
 
-const metainformation = {
+const metainformation: BlogPostMetainformation = {
   title: "An analogy on JavaScript's single-threaded asynchronicity.",
   description:
     "Uncover the secrets of JavaScript's asynchronicity and the event loop in this blog post, where we use a playful analogy featuring a waitperson, kitchen, and counter. Learn how parallel steps keep your requests smooth and why avoiding long tasks is crucial. Find out why some websites freeze! 🤓 #JavaScript #AsynchronousProgramming #EventLoop",
-  date: '2023-03-03',
+  date: new Date('2023-03-03'),
   lengthInMinutes: 3,
 };
 
@@ -25,7 +26,7 @@ const metadata: Metadata = {
     siteName: 'segebre.dev',
     locale: 'en_US',
     type: 'article',
-    publishedTime: metainformation.date,
+    publishedTime: metainformation.date.toISOString().slice(0, 10),
     authors: ['Juan Enrique Segebre Zaghmout'],
     section: 'JavaScript',
     tags: [
